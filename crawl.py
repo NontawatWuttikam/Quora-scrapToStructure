85cf11f52a9a10533b93d5d60fb266e55440d5c4
nontawatwuttikam <nontawatwuttikam@gmail.com>
2021-09-15 00:31:07 -0700

    Implemented question divs extractor

--- crawl.py
+++ crawl.py
@@ -1,64 +1,62 @@
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# import time
-# from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.by import By
 
-# options = webdriver.ChromeOptions() 
+options = webdriver.ChromeOptions() 
 
-# options.add_argument('--user-data-dir=C:/Users/Lenovo/AppData/Local/Google/Chrome/User Data')
-# # options.add_argument('--profile-directory=Profile 1')
+options.add_argument('--user-data-dir=C:/Users/Lenovo/AppData/Local/Google/Chrome/User Data')
+# options.add_argument('--profile-directory=Profile 1')
 
-# driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
 
-# question_class_name = "q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--underline Link___StyledBox-t2xg9c-0 roKEj"
-# div_answer_amount = "q-text qu-dynamicFontSize--regular qu-medium qu-color--gray_dark qu-passColorToLinks"
-# div_answer_box = "CssComponent-sc-1oskqb9-0 cXjXFI"
-# div_answer_text = "q-text"
-# div_answer_nodelist_index = 7 #Where answer text (qtext) is at in the answer box
-# button_comment = "q-click-wrapper ClickWrapper___StyledClickWrapperBox-zoqi4f-0 bIwtPb base___StyledClickWrapper-lx6eke-1 laIUvT   qu-active--bg--darken qu-active--textDecoration--none qu-borderRadius--pill qu-alignItems--center qu-justifyContent--center qu-whiteSpace--nowrap qu-userSelect--none qu-display--inline-flex qu-tapHighlight--white qu-textAlign--center qu-cursor--pointer qu-hover--bg--darken qu-hover--textDecoration--none"
+question_class_name = "q-box qu-display--block qu-cursor--pointer qu-hover--textDecoration--underline Link___StyledBox-t2xg9c-0 roKEj"
+div_answer_amount = "q-text qu-dynamicFontSize--regular qu-medium qu-color--gray_dark qu-passColorToLinks"
+div_answer_box = "CssComponent-sc-1oskqb9-0 cXjXFI"
+div_answer_text = "q-text"
+div_answer_nodelist_index = 7 #Where answer text (qtext) is at in the answer box
+button_comment = "q-click-wrapper ClickWrapper___StyledClickWrapperBox-zoqi4f-0 bIwtPb base___StyledClickWrapper-lx6eke-1 laIUvT   qu-active--bg--darken qu-active--textDecoration--none qu-borderRadius--pill qu-alignItems--center qu-justifyContent--center qu-whiteSpace--nowrap qu-userSelect--none qu-display--inline-flex qu-tapHighlight--white qu-textAlign--center qu-cursor--pointer qu-hover--bg--darken qu-hover--textDecoration--none"
 
-# def click_comment_btn():
-#     return driver.execute_script("return " + open("./js/click_comment_btn.js").read())
+def click_comment_btn():
+    return driver.execute_script("return " + open("./js/click_comment_btn.js").read())
 
-# def extract_answer_div():
-#     driver.execute_script(open("./js/extract_question.js").read())
-#     return driver.execute_script("return get_all_answer_div()")
+def extract_answer_div():
+    driver.execute_script(open("./js/extract_question.js").read())
+    return driver.execute_script("return get_all_answer_div()")
 
 
-# def scroll(max_iter = None):
-#     lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-#     match=False
-#     count = 0
-#     while(match==False):
-#         lastCount = lenOfPage
-#         time.sleep(3)
-#         lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-#         if lastCount==lenOfPage:
-#             match=True
-#         if max_iter is not None:
-#             if max_iter == count: break
-#         count += 1
-#     return
+def scroll(max_iter = None):
+    lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+    count = 0
+    while(match==False):
+        lastCount = lenOfPage
+        time.sleep(3)
+        lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        if lastCount==lenOfPage:
+            match=True
+        if max_iter is not None:
+            if max_iter == count: break
+        count += 1
+    return
 
-# def question_scrapper(keyword,max_iter = None):
-#     driver.get("https://www.quora.com/search?q="+keyword)
-#     scroll(max_iter)
-#     element = driver.execute_script("return document.getElementsByClassName(\"" + question_class_name + "\"" + ")")
-#     result = []
-#     for e in element:
-#         result.append({"question":e.text, "url":e.get_attribute('href')})
-#     return result
+def question_scrapper(keyword,max_iter = None):
+    driver.get("https://www.quora.com/search?q="+keyword)
+    scroll(max_iter)
+    element = driver.execute_script("return document.getElementsByClassName(\"" + question_class_name + "\"" + ")")
+    result = []
+    for e in element:
+        result.append({"question":e.text, "url":e.get_attribute('href')})
+    return result
 
-# result = question_scrapper("covid",1)
+result = question_scrapper("covid",1)
 
-# for question in result:
-#     driver.get(question["url"])
-#     scroll(1)
-#     # click_comment_btn()
-#     print(type(extract_answer_div()))
+for question in result:
+    driver.get(question["url"])
+    scroll(1)
+    # click_comment_btn()
+    print(extract_answer_div())
 
     
 
-# print(result)
-
-print("js", open("./js/extract_question.js").read())
+print(result)
--- js/extract_question.js
+++ js/extract_question.js
@@ -1,19 +1,19 @@
 xpath_template = "//*[@id=\"mainContent\"]/div[2]/div[<NUM>]"
 xpath_unique_answer = "//*[@id=\"mainContent\"]/div[2]/div[<NUM>]/div/div/div/div/div/div/div"
 
-function getElementByXPath(path) { 
+window.getElementByXPath = (path)=> { 
     return (new XPathEvaluator()) 
         .evaluate(path, document.documentElement, null, 
                         XPathResult.FIRST_ORDERED_NODE_TYPE, null) 
         .singleNodeValue; 
 } 
 
-function is_answer(idx) {
+window.is_answer = (idx) => {
     xp = xpath_unique_answer.replace("<NUM>",idx.toString())
     return getElementByXPath(xp) != null
 }
 
-function get_all_answer_div() {
+window.get_all_answer_div = () => {
     var ans = []
     list_ele = getElementByXPath("//*[@id=\"mainContent\"]/div[2]").children
     for(var i = 0 ; i < list_ele.length ; i++) {
